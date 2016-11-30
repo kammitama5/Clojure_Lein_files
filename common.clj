dfebbe4c97b121b1eb9a4dd9fd0e6a77a18bc0c7
kammitama5 <krystal.maughan@gmail.com>
2016-11-29 22:30:04 -0600

    notes common functions

---
+++ common.clj
@@ -0,0 +1,11 @@
+;; count => size of collection
+;; conj => "conjoin" or add a new value
+;; into => pour one collection into another
+
+;;Only for vectors and lists
+;; peek => retrieve the value at the insertion pt
+;; pop => remove last value
+
+;;Only for sets
+;;disj => disjoin => remove a value
+;;contains? => does it contain this key?
